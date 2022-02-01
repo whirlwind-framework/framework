f68c48c5a0c573610a2ff3d3d9a967c6782e4da2
Zakhar <zakhar.huzenko@business-class.com>
2022-02-01 16:27:48 +0200

    add JsonDecodeMiddleware, fix console request

---
+++ src/App/Http/Middleware/JsonDecodeMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Whirlwind\App\Http\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class JsonDecodeMiddleware implements MiddlewareInterface
+{
+    private const DEFAULT_JSON_DEPTH = 512;
+
+    /** @throws \JsonException */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $handler->handle($request->withParsedBody($this->parseBody($request)));
+    }
+
+    /** @throws \JsonException */
+    private function parseBody(ServerRequestInterface $request)
+    {
+        if (!$request->hasHeader('Content-Type')) {
+            return $request->getParsedBody();
+        }
+
+        if (strpos($request->getHeaderLine('Content-Type'), 'application/json') === false) {
+            return $request->getParsedBody();
+        }
+
+        return json_decode(
+            (string)$request->getBody(),
+            true,
+            self::DEFAULT_JSON_DEPTH,
+            JSON_THROW_ON_ERROR
+        );
+    }
+}
