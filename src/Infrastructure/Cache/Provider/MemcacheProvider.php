2bde39eadb3af759e3794453c08032c2850bc976
Zakhar <zakhar.huzenko@business-class.com>
2022-02-07 09:30:33 +0200

    improve cache providers

--- src/Infrastructure/Cache/Provider/MemcacheProvider.php
+++ src/Infrastructure/Cache/Provider/MemcacheProvider.php
@@ -9,23 +9,27 @@ use Whirlwind\Infrastructure\Cache\ProviderInterface;
 
 class MemcacheProvider extends AbstractCacheProvider
 {
+    private const DEFAULT_SERVER_CONFIG = [
+        'host' => '127.0.0.1',
+        'port' => 11211,
+        'persistent' => true,
+        'weight' => 0,
+    ];
+
     public function __construct(array $config = [])
     {
         if (empty($config['servers'])) {
-            $config['servers'] = [[
-                'host' => '127.0.0.1',
-                'port' => 11211,
-            ]];
+            $config['servers'] = [static::DEFAULT_SERVER_CONFIG];
         }
 
         $this->cache = new Memcache();
 
         foreach ($config['servers'] as $server) {
             $this->cache->addServer(
-                $server['host'],
-                $server['port'],
-                true,
-                ($server['weight'] ?? 1)
+                $server['host'] ?? static::DEFAULT_SERVER_CONFIG['host'],
+                $server['port'] ?? static::DEFAULT_SERVER_CONFIG['port'],
+                $server['persistent'] ?? static::DEFAULT_SERVER_CONFIG['persistent'],
+                $server['weight'] ?? static::DEFAULT_SERVER_CONFIG['weight']
             );
         }
     }
